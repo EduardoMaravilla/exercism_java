5f996d209731b4ebef54e0d66ae1cb968bbbd3e7
Eduardo Maravilla <130570452+EduardoMaravilla@users.noreply.github.com>
2023-06-06 11:21:34 -0600

    Add files via upload

---
+++ acronym/src/main/java/Acronym.java
@@ -0,0 +1,19 @@
+class Acronym {
+    private String frase;
+
+    Acronym(String phrase) {
+        frase = phrase;
+    }
+
+    String get() {
+        StringBuilder acr = new StringBuilder();
+        String[] cadena = frase.split("[\\s-]+");
+        int num = cadena.length;
+        for(int i = 0; i < num; i++) {
+            String primer = cadena[i];
+            primer = primer.replaceAll("_", "");
+            acr.append(primer.substring(0, 1));
+        }
+        return acr.toString().toUpperCase();
+    }
+}
